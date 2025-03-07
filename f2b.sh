a8eefa9e579c2b87033168a33f741293b980e2d0
chrimast <119728939+chrimast@users.noreply.github.com>
2025-03-07 22:05:00 +0800

    更新 f2b.sh

--- f2b.sh
+++ f2b.sh
@@ -35,7 +35,7 @@ echo "CF地址范围添加完成"
 
 # ------------------------------
 # 2. 在文件 action.d/cloudflare.conf 中添加CF配置
-sed -i -e "/^cfuser =/ s/$/ chrimast@gmail.com/" -e "/^cftoken =/ s/$/ c85cf3a6a278ab2fb70629072677ca58b4ff3/" /etc/fail2ban/action.d/cloudflare.conf
+sudo sed -i -e "s/^cfuser = .*/cfuser = chrimast@gmail.com/" -e "s/^cftoken = .*/cftoken = c85cf3a6a278ab2fb70629072677ca58b4ff3/" /etc/fail2ban/action.d/cloudflare.conf
 echo -e "\ncftarget = ip" >> /etc/fail2ban/action.d/cloudflare.conf
 echo -e "\ncftarget_v6 = ipv6" >> /etc/fail2ban/action.d/cloudflare.conf
 
