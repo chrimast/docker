da2a26fbea2e48a245adb9611cf47eb93d346f64
chrimast <119728939+chrimast@users.noreply.github.com>
2025-03-07 21:44:28 +0800

    更新 f2b.sh

--- f2b.sh
+++ f2b.sh
@@ -2,47 +2,36 @@
 # ---------------------------------
 # 1. 在 nginx.conf 中添加CF的IP库白名单
 sed -i '/http {/r /dev/stdin' /opt/1panel/apps/openresty/openresty/conf/nginx.conf <<EOF
+
+    # CloudFlare IP范围
     set_real_ip_from 103.21.244.0/22;
-    set_real_ip_from 203.0.113.0/24;
-    real_ip_header X-Forwarded-For;
+    set_real_ip_from 103.22.200.0/22;
+    set_real_ip_from 103.31.4.0/22;
+    set_real_ip_from 104.16.0.0/13;
+    set_real_ip_from 104.24.0.0/14;
+    set_real_ip_from 108.162.192.0/18;
+    set_real_ip_from 131.0.72.0/22;
+    set_real_ip_from 141.101.64.0/18;
+    set_real_ip_from 162.158.0.0/15;
+    set_real_ip_from 172.64.0.0/13;
+    set_real_ip_from 173.245.48.0/20;
+    set_real_ip_from 188.114.96.0/20;
+    set_real_ip_from 190.93.240.0/20;
+    set_real_ip_from 197.234.240.0/22;
+    set_real_ip_from 198.41.128.0/17;
+    set_real_ip_from 2400:cb00::/32;
+    set_real_ip_from 2606:4700::/32;
+    set_real_ip_from 2803:f800::/32;
+    set_real_ip_from 2405:b500::/32;
+    set_real_ip_from 2405:8100::/32;
+    set_real_ip_from 2a06:98c0::/29;
+    set_real_ip_from 2c0f:f248::/32;
+    real_ip_header CF-Connecting-IP;
     real_ip_recursive on;
+
 EOF
-# 自定义CF真实IP
-content="# CloudFlare IP
-set_real_ip_from 103.21.244.0/22;
-set_real_ip_from 103.22.200.0/22;
-set_real_ip_from 103.31.4.0/22;
-set_real_ip_from 104.16.0.0/13;
-set_real_ip_from 104.24.0.0/14;
-set_real_ip_from 108.162.192.0/18;
-set_real_ip_from 131.0.72.0/22;
-set_real_ip_from 141.101.64.0/18;
-set_real_ip_from 162.158.0.0/15;
-set_real_ip_from 172.64.0.0/13;
-set_real_ip_from 173.245.48.0/20;
-set_real_ip_from 188.114.96.0/20;
-set_real_ip_from 190.93.240.0/20;
-set_real_ip_from 197.234.240.0/22;
-set_real_ip_from 198.41.128.0/17;
 
-set_real_ip_from 2400:cb00::/32;
-set_real_ip_from 2606:4700::/32;
-set_real_ip_from 2803:f800::/32;
-set_real_ip_from 2405:b500::/32;
-set_real_ip_from 2405:8100::/32;
-set_real_ip_from 2a06:98c0::/29;
-set_real_ip_from 2c0f:f248::/32;
-
-real_ip_header    CF-Connecting-IP;"
-
-# 要插入的行号
-line_number=$(grep -n "1pwaf/data/conf/waf.conf" "$file_path" | cut -d':' -f1)
-
-# 使用 sed 在指定行的下面添加内容
-sed -i "${line_number}a\\
-$content" "$file_path"
-
-echo "内容添加完成"
+echo "CF地址范围添加完成"
 
 # ------------------------------
 # 2. 在文件 action.d/cloudflare.conf 中添加CF配置
@@ -51,6 +40,7 @@ sed -i "/^cftoken =/ s/$/ c85cf3a6a278ab2fb70629072677ca58b4ff3/" /etc/fail2ban/
 echo -e "\ncftarget = ip" >> /etc/fail2ban/action.d/cloudflare.conf
 echo -e "\ncftarget_v6 = ipv6" >> /etc/fail2ban/action.d/cloudflare.conf
 
+echo "CF配置内容添加完成"
 # -------------------------------
 # 3. 创建并写入 filter.d/nginx-cc.conf 文件
 cat > /etc/fail2ban/filter.d/nginx-cc.conf <<EOF
@@ -65,6 +55,7 @@ ignoreregex = ^.*(\/(?:robots\.txt|favicon\.ico|.*\.(?:jpg|png|gif|jpeg|svg|webp
 
 EOF
 
+echo "nginx-cc.conf文件创建完成"
 # -------------------------------
 # 4. 追加配置到 jail.local 文件
 cat >> /etc/fail2ban/jail.local <<EOF
@@ -158,4 +149,4 @@ logpath = /opt/1panel/apps/openresty/openresty/log/*.log
 
 EOF
 
-echo "配置添加完成"
+echo "jail.local配置完成"
