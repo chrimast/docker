15f32f0ac406a064a750efd29941963d8b29da0d
chrimast <119728939+chrimast@users.noreply.github.com>
2025-03-07 18:55:31 +0800

    更新 f2b.sh

--- f2b.sh
+++ f2b.sh
@@ -3,7 +3,6 @@
 # --------------------------------------
 # 1. 在 nginx.conf 中添加CF的IP库白名单
 file_path="/opt/1panel/apps/openresty/openresty/conf/nginx.conf"
-
 # 要添加的内容
 content="# CloudFlare IP
 set_real_ip_from 103.21.244.0/22;
